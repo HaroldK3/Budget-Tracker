89d3fd20b9b97574884abf38f6f71bc0b4456b03
HaroldK3 <haroldk@etsu.edu>
2026-01-22 11:51:30 -0500

    Adding API folder

---
+++ API/routes/user.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from db import get_db
+from API.models import User
+
+router = APIRouter(prefix="/user", tags=["User"])
+
+# Login
+
+# Expenses
+
+# Income
+
+# Goals
+
+# Categories
+
+# Budget
+
+# Bills 
+
+# Tips 
+
+# Overspending
+
+# Profile
