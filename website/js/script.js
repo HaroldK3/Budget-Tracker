1baa3c014f17205fe3da2f38fcd9fb1d4dec0529
Aurora Liles <LILESA@ETSU.EDU>
2026-02-12 12:11:24 -0500

    basic login form and js script built- needs testing

---
+++ website/js/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    //login logic
+    const loginForm = document.getElementById("login-form");
+    if (loginForm)
+    {
+        loginForm.addEventListener("submit", async (e) => {
+            e.preventDefault();
+            
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+
+            const url = new URL("http://127.0.0.1:8000/user/login");
+            url.searchParams.append("email", email);
+            url.searchParams.append("password", password);
+
+            const res = await fetch(url, { method: "POST" });
+            const data = await res.json();
+
+            if(!res.ok)
+            {
+                alert(data.detail || "Login failed");
+                return;
+            }
+
+            alert(data.message);
+        });
+    }
+})
